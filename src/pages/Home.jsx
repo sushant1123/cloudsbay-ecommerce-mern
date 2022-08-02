9aad3eafb33ffceda4b707e1215b78ddca58ca29
Sushant Bahirat <sushantbahirat40@gmail.com>
2022-08-03 00:24:46 +0530

    If user not logged in, navigate to login page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux/es/exports";
 
-const Home = () => {
+const Home = ({ history }) => {
+	const { user } = useSelector((state) => state);
+
+	useEffect(() => {
+		if (!user || !user.token) history.push("/login");
+	}, [user, history]);
+
 	return (
 		<div>
 			<p>Home</p>
