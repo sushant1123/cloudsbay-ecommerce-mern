2c7a4857382dd2468277a128d216f5779aba9546
Sushant Bahirat <sushantbahirat40@gmail.com>
2022-09-14 22:49:32 +0530

    fetch all products in admin dashboard.

--- src/api's/product.js
+++ src/api's/product.js
@@ -8,6 +8,6 @@ export const createProduct = async (product, authtoken) => {
 	return await axios.post(`${REACT_APP_API_URL}/product`, { ...product }, { headers: { authtoken } });
 };
 
-export const getProducts = async () => {
-	return await axios.get(`${REACT_APP_API_URL}/products`);
+export const getProducts = async (count) => {
+	return await axios.get(`${REACT_APP_API_URL}/products/${count}`);
 };
